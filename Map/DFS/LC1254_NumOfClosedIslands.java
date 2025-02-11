1d4957bc2d0d7f60d28ddc18828a3a31cd6a85a7
Seaman-Li <844404421@qq.com>
2025-02-11 01:24:57 -0800

    new

---
+++ Map/DFS/LC1254_NumOfClosedIslands.java
@@ -0,0 +1,54 @@
+package Map.DFS;
+
+public class LC1254_NumOfClosedIslands {
+
+    public int closedIsland(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        for(int j = 0; j < n; j++){
+            dfs(grid, 0, j);// 把靠上边界的岛屿淹掉
+            dfs(grid, m - 1, j);// 把靠上边界的岛屿淹掉
+        }
+        for(int i = 0; i < m; i++){
+            dfs(grid, i, 0);// 把靠左边界的岛屿淹掉
+            dfs(grid, i, n - 1);// 把靠右边界的岛屿淹掉
+        }
+        int res = 0;
+        // 遍历 grid，剩下的岛屿都是封闭岛屿
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 0){
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+
+    // 从 (i, j) 开始，将与之相邻的陆地都变成海水
+    void dfs(int[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+        if (i < 0 || j < 0 || i >= m || j >= n) {
+            return;
+        }
+        if (grid[i][j] == 1) { // 已经是海水了
+            return;
+        }
+        // 将 (i, j) 变成海水
+        grid[i][j] = 1;
+        // 淹没上下左右的陆地
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {{1,1,1,1,1,1,1,0},{1,0,0,0,0,1,1,0},{1,0,1,0,1,1,1,0},{1,0,0,0,0,1,0,1},{1,1,1,1,1,1,1,0}};
+        LC1254_NumOfClosedIslands sol = new LC1254_NumOfClosedIslands();
+        System.out.println(sol.closedIsland(grid));
+    }
+}
--- Map/DFS/LC200_NumOfIsland.java
+++ Map/DFS/LC200_NumOfIsland.java
@@ -25,12 +25,33 @@ public class LC200_NumOfIsland {
         if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == '0') {
             return;
         }
-        // 将当前格子标记为已访问
+        // 将当前格子标记为已访问.dfs 函数遍历到值为 0 的位置会直接返回，所以只要把经过的位置都设置为 0，就可以起到不走回头路的作用
         grid[row][col] = '0';
         // 遍历上下左右四个方向
         dfs(grid, row - 1, col); // 上
         dfs(grid, row + 1, col); // 下
         dfs(grid, row, col - 1); // 左
         dfs(grid, row, col + 1); // 右
+    }
+
+
+
+    public static void main(String[] args) {
+        char[][] grid = new char[4][];
+        grid[0] = "11000".toCharArray();
+        grid[1] = "11000".toCharArray();
+        grid[2] = "00100".toCharArray();
+        grid[3] = "00011".toCharArray();
+
+//        for(int i = 0;i < grid.length;i++) {
+//            for(int j = 0;j < grid[0].length;j++) {
+//                System.out.print(grid[i][j] + " ");
+//            }
+//        }
+
+        LC200_NumOfIsland sol = new LC200_NumOfIsland();
+
+        System.out.println(sol.numIslands(grid));
+
     }
 }
