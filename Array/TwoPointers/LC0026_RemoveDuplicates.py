7f4d3bf098767eea1201cd0c3b09dedb12becf29
Seaman-Li <844404421@qq.com>
2026-05-19 01:40:11 +0800

    upgrate

---
+++ Array/TwoPointers/LC0026_RemoveDuplicates.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        slow = 0
+        fast = 1
+        while(fast < len(nums)):
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+            fast += 1
+        return slow + 1
+
+
+def main():
+    nums = [0,0,1,1,1,2,2,3,3,4]
+    solution = Solution()
+    print(solution.removeDuplicates(nums))
+
+if __name__ == '__main__':
+    main()
